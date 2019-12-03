732125c40724a49fa309f7927c7cede6df3e97e8
GuilhermeDellatin <guidellatin@gmail.com>
2019-12-03 13:13:46 -0300

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,31 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+
+    // https://stackoverflow.com/questions/5762491/how-to-print-color-in-console-using-system-out-println
+    //Cores do texto
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+    //Cores do Fundo
+    public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+    public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+    public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+    public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+    public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+    public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+    public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
     //Método para imprimir o formato tabuleiro
     public static void printBoard(ChessPiece[][] pieces){
         for(int i = 0; i < pieces.length; i++){
@@ -17,6 +40,7 @@ public class UI {
     }
 
     //Método auxiliar para imprimir uma peça
+    /*
     private static void printPiece(ChessPiece piece){
         if(piece == null){
             System.out.print("-");
@@ -25,6 +49,21 @@ public class UI {
             System.out.print(piece);
         }
         //Imprimir um espaço em branco para que as peças não fiquem grudadas umas nas outras
+        System.out.print(" ");
+    }*/
+
+    private static void printPiece(ChessPiece piece) {
+        if (piece == null) {
+            System.out.print("-");
+        }
+        else {
+            if (piece.getColor() == Color.WHITE) { //Antes de imprimir uma peça vamos testar se é branca ou preta
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
         System.out.print(" ");
     }
 
